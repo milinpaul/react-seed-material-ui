99ac99a963e67ceb01c28fa9e6e3aae24757d853
Milin Paul <milin.paul@experionglobal.com>
2020-01-21 16:48:53 +0530

    minor changes

--- src/App/Pages/Home/index.js
+++ src/App/Pages/Home/index.js
@@ -2,9 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const PrimaryTitle = styled.h1`
-  font-size: 1.5em;
+  font-size: 1.5rem;
   text-align: center;
-  color: palevioletred;
 `
 
 const index = () => {
